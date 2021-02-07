f4d62b099635bb523cd66afc1ed39dd60b05ad9c
hackerton <HackerTon@users.noreply.github.com>
2021-02-07 21:09:30 +0800

    added git ignore
    Signed-off-by: hackerton <wizardlordlam@gmail.com>

--- pretrained_example.py
+++ pretrained_example.py
@@ -15,17 +15,22 @@ import dnnlib
 import dnnlib.tflib as tflib
 import config
 
+
 def main():
     # Initialize TensorFlow.
     tflib.init_tf()
 
     # Load pre-trained network.
-    url = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ' # karras2019stylegan-ffhq-1024x1024.pkl
-    with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
+    url = "https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ"  # karras2019stylegan-ffhq-1024x1024.pkl
+    # with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
+    #     _G, _D, Gs = pickle.load(f)
+    #     # _G = Instantaneous snapshot of the generator. Mainly useful for resuming a previous training run.
+    #     # _D = Instantaneous snapshot of the discriminator. Mainly useful for resuming a previous training run.
+    #     # Gs = Long-term average of the generator. Yields higher-quality results than the instantaneous snapshot.
+
+    # manually override loading
+    with open("karras2019stylegan-ffhq-1024x1024.pkl", "rb") as f:
         _G, _D, Gs = pickle.load(f)
-        # _G = Instantaneous snapshot of the generator. Mainly useful for resuming a previous training run.
-        # _D = Instantaneous snapshot of the discriminator. Mainly useful for resuming a previous training run.
-        # Gs = Long-term average of the generator. Yields higher-quality results than the instantaneous snapshot.
 
     # Print network details.
     Gs.print_layers()
@@ -36,12 +41,15 @@ def main():
 
     # Generate image.
     fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
-    images = Gs.run(latents, None, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
+    images = Gs.run(
+        latents, None, truncation_psi=0.7, randomize_noise=True, output_transform=fmt
+    )
 
     # Save image.
     os.makedirs(config.result_dir, exist_ok=True)
-    png_filename = os.path.join(config.result_dir, 'example.png')
-    PIL.Image.fromarray(images[0], 'RGB').save(png_filename)
+    png_filename = os.path.join(config.result_dir, "example.png")
+    PIL.Image.fromarray(images[0], "RGB").save(png_filename)
+
 
 if __name__ == "__main__":
     main()
